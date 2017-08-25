a148aaa63570c770281b66ca8c1656e1bcb7f8f2
zsinx6 <lucassin_x@hotmail.com>
2017-08-25 17:50:14 -0300

    Add Brandes algorithm

--- brandes.cpp
+++ brandes.cpp
@@ -4,6 +4,7 @@
 #include <queue>
 #include <stack>
 #include <vector>
+#include <iostream>
 #include "graph.h"
 
 void read_graph(graph **g, FILE *input){
@@ -13,6 +14,85 @@ void read_graph(graph **g, FILE *input){
         add_edge(g, node_a, node_b); 
         add_edge(g, node_b, node_a); 
     }
+}
+
+void brandes(graph **g, double *Cb){
+    int n_nodes = -1, *d = NULL, *sigma = NULL, v = -1, w = -1;
+    int i = -1, s = -1;
+    double *delta = NULL;
+    std::stack<int> S;
+    std::queue<int> Q;
+    std::vector< std::vector<int> > P;
+    adjacent_node *node = NULL;
+    n_nodes = (*g)->n_nodes;
+    sigma = (int*) malloc(n_nodes*sizeof(int));
+    delta = (double*) malloc(n_nodes*sizeof(double));
+    d = (int*) malloc(n_nodes*sizeof(int));
+    for(int i=0; i < n_nodes; i++){
+        P.push_back(std::vector<int>());
+    }
+    for(s=0; s < n_nodes; s++){
+        //empty stack
+        while(!(S.empty())){
+            S.pop();
+        }
+        //empty list, for all
+        for(i=0; i < n_nodes; i++){
+            while(!(P[i].empty())){
+                P[i].pop_back();
+            }
+        }
+        for(i=0; i < n_nodes; i++){
+            sigma[i] = 0;
+            d[i] = -1;
+        }
+        sigma[s] = 1;
+        d[s] = 0;
+        //empty queue
+        while(!(Q.empty())){
+            Q.pop();
+        }
+        Q.push(s);
+        while(!(Q.empty())){
+            v = Q.front();
+            Q.pop();
+            S.push(v);
+
+            //for node w of neighbor of node v
+            //get the first item, then iterates until the last (node==NULL)
+            node = (*g)->nodes[v]->edges;
+            while(node != NULL){
+                w = node->id;
+                if(d[w] < 0){
+                    Q.push(w);
+                    d[w] = d[v] + 1;
+                }
+                if(d[w] == d[v] + 1){
+                    sigma[w] += sigma[v];
+                    P[w].push_back(v);
+                }
+                node = node->next;
+            }
+        }
+    
+        for(i=0; i < n_nodes; i++){
+            delta[i] = 0;
+        }
+        while(!(S.empty())){
+            w = S.top();
+            S.pop();
+            //for all nodes in P[w]
+            for(std::vector<int>::size_type j = 0; j != P[w].size(); j++){
+                delta[P[w][j]] += ((1.0 * sigma[P[w][j]]/sigma[w]) * (1 + delta[w]));
+            }
+            if(w != s){
+                Cb[w] += delta[w];
+            }
+        }
+    }
+    free(sigma);
+    free(delta);
+    free(d);
 }
 
 int main(int argc, char *argv[]){
@@ -49,6 +129,11 @@ int main(int argc, char *argv[]){
             node = node->next;
         }
         printf("\n");
+    }
+
+    brandes(&g, Cb);
+    for(int i=0; i < n_nodes; i++){
+        printf("%lf\n", Cb[i]);
     }
 
     fclose(input);
